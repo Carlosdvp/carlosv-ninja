84bd0e99a6b3381d05aa871a732f766cf3402c4a
Carlos V <carlosdvp.tx@gmail.com>
2018-10-21 20:13:31 -0400

    added the images for the gallery and some basic styling for the component.

--- Re-factor/my-ninja-project/src/components/Gallery.vue
+++ Re-factor/my-ninja-project/src/components/Gallery.vue
@@ -1,99 +1,91 @@
 <template>
 
 <!-- Portfolio Gallery thumbnail section -->
-  <div class="main-gallery m-3" id="portfolio-thumbnail">
+  <div class="main-gallery" id="portfolio-thumbnail">
 
-    <div class="card-deck">
-      <div class="card text-center col-sm-12 col-md-6 img-thumbnail gallery-image">
-        <h4 class="card-header">Arquitectura Apocaliptica Srl</h4>
-        <a href="https://carlosdvp.github.io/Apocaliptica/" data-toggle="tooltip" title="Arquitectura Apocaliptica - Top Section" target="_blank">
-          <img class="card-img-top rounded img-fluid img-thumbnail gallery-image" src="dist/images/Apocaliptica-1.jpg" alt="project image" id="gallery-image-1">
-        </a>
-        <div class="card-body">
-          <h5 class="">Centro de Diseno y Construccion</h5>
-          <p class="card-text">Santa Cruz de la Sierra, Bolivia</p>
-        </div>
-        <div class="footer">
-          <a href="http://www.arquitecturaapocaliptica.com" class="btn btn-primary" title="Arquitectura Apocaliptica" target="_blank">Visit the Website</a>
-          
-        </div>
+    <div>
+      <h4 class="">Arquitectura Apocaliptica Srl</h4>
+      <a href="https://carlosdvp.github.io/Apocaliptica/" data-toggle="tooltip" title="Arquitectura Apocaliptica - Top Section" target="_blank">
+        <img class="gallery-image img-thumbnail" src="../assets/Apocaliptica-1.jpg" alt="project image" id="gallery-image-1">
+      </a>
+      <div>
+        <h5>Centro de Diseno y Construccion</h5>
+        <p>Santa Cruz de la Sierra, Bolivia</p>
       </div>
-
-      <div class="card text-center col-sm-12 col-md-6 gallery-image img-thumbnail">
-        <h5 class="card-header">Arquitectura Apocaliptica Srl</h5>
-        <a href="https://carlosdvp.github.io/Apocaliptica/" data-toggle="tooltip" title="Arquitectura Apocaliptica" target="_blank">
-          <img class="card-img-top rounded img-fluid img-thumbnail gallery-image" src="dist/images/Apocaliptica-2.jpg" alt="project image" id="gallery-image-1">
-        </a>
-        <div class="card-body">
-          <h5 class="">Github Repository</h5>
-          <p class="card-text">Source code and project details.</p>
-        </div>
-        <div class="footer">
-          <a href="https://carlosdvp.github.io/Apocaliptica/" class="btn btn-primary" title="Arquitectura Apocaliptica" target="_blank">Visit the Project Repo</a>
-          
-        </div>
-      </div>
-
-      <div class="card text-center col-sm-12 col-md-6 gallery-image img-thumbnail">
-        <h4 class="card-header">DragonBall SVG Animation</h4>
-        <a href="https://carlosdvp.github.io/DBZ-Animation/" data-toggle="tooltip" title="DragonBall SVG Animation" target="_blank">
-          <img class="card-img-top rounded img-fluid img-thumbnail gallery-image" src="dist/images/SVG-Animation.jpg" alt="DBZ project image" id="gallery-image-1">
-        </a>
-        <div class="card-body">
-          <h5 class="">CSS Animations</h5>
-          <p class="card-text">Animating SVG images with CSS</p>
-        </div>
-        <div class="footer">
-          <a href="https://carlosdvp.github.io/DBZ-Animation/" class="btn btn-primary" title="DragonBall SVG Animation" target="_blank">Visit the Project</a>
-          
-        </div>
+      <div class="footer">
+        <a href="http://www.arquitecturaapocaliptica.com" class="btn btn-primary" title="Arquitectura Apocaliptica" target="_blank">Visit the Website</a>
+        
       </div>
     </div>
 
-    <div class="card-deck">
-      <div class="card text-center col-sm-12 col-md-6 gallery-image img-thumbnail">
-        <h4 class="card-header">Responsive Portfolio</h4>
-        <a href="http://carlosdvp.github.io/Responsive-Portfolio/" data-toggle="tooltip" title="Responsive Portfolio Project" target="_blank">
-          <img class="card-img-top rounded img-fluid img-thumbnail gallery-image" src="dist/images/Responsive-Portfolio.jpg" alt="Responsive Portfolio Project" id="gallery-image-1">
-        </a>
-        <div class="card-body">
-          <h5 class="">Responsive Portfolio Project</h5>
-          <p class="card-text">Santa Cruz de la Sierra, Bolivia</p>
-        </div>
-        <div class="footer">
-          <a href="http://carlosdvp.github.io/Responsive-Portfolio/" class="btn btn-primary" title="Responsive Portfolio Project" target="_blank">Visit the Project</a>
-          
-        </div>
+    <div>
+      <h5>Arquitectura Apocaliptica Srl</h5>
+      <a href="https://carlosdvp.github.io/Apocaliptica/" data-toggle="tooltip" title="Arquitectura Apocaliptica" target="_blank">
+        <img class="gallery-image img-thumbnail" src="../assets/Apocaliptica-2.jpg" alt="project image">
+      </a>
+      <div>
+        <h5>Github Repository</h5>
+        <p>Source code and project details.</p>
       </div>
-
-      <div class="card text-center col-sm-12 col-md-6 gallery-image img-thumbnail">
-        <h4 class="card-header">Website Project 3</h4>
-        <a href="https://carlosdvp.github.io/Website-project-3/" data-toggle="tooltip" title="Portfolio Project Number 3" target="_blank">
-          <img class="card-img-top rounded img-fluid img-thumbnail gallery-image" src="dist/images/Website-project-3.jpg" alt="Portfolio Poject 3" id="gallery-image-1">
-        </a>
-        <div class="card-body">
-          <h5 class="">Portfolio Website Number 3</h5>
-          <p class="card-text">Falls Church, VA</p>
-        </div>
-        <div class="footer">
-          <a href="https://carlosdvp.github.io/Website-project-3/" class="btn btn-primary" title="Portfolio Project Number 3" target="_blank">Visit the Project</a>
-          
-        </div>
+      <div>
+        <a href="https://carlosdvp.github.io/Apocaliptica/" class="btn btn-primary" title="Arquitectura Apocaliptica" target="_blank">Visit the Project Repo</a>
       </div>
+    </div>
 
-      <div class="card text-center col-sm-12 col-md-6 gallery-image img-thumbnail">
-        <h4 class="card-header">JS Basic Chatbot Project</h4>
-        <a href="https://carlosdvp.github.io/First-bot-Chatbot/" data-toggle="tooltip" title="Basic JS chatbot" target="_blank">
-          <img class="card-img-top rounded img-fluid img-thumbnail gallery-image" src="dist/images/basic-chatbot.png" alt="Js chatbot Project" id="gallery-image-1">
-        </a>
-        <div class="card-body">
-          <h5 class="">JS Project</h5>
-          <p class="card-text">Basic Chatbot App written in JavaScript</p>
-        </div>
-        <div class="footer">
-          <a href="http://carlosdvp.github.io/Udacity-Resume-Project/" class="btn btn-primary" title="Udacity Js Project" target="_blank">Visit the Project</a>
-          
-        </div>
+    <div>
+      <h4>DragonBall SVG Animation</h4>
+      <a href="https://carlosdvp.github.io/DBZ-Animation/" data-toggle="tooltip" title="DragonBall SVG Animation" target="_blank">
+        <img class="gallery-image img-thumbnail" src="../assets/SVG-Animation.jpg" alt="DBZ project image">
+      </a>
+      <div>
+        <h5>CSS Animations</h5>
+        <p>Animating SVG images with CSS</p>
+      </div>
+      <div>
+        <a href="https://carlosdvp.github.io/DBZ-Animation/" class="btn btn-primary" title="DragonBall SVG Animation" target="_blank">Visit the Project</a>
+      </div>
+    </div>
+
+    <div>
+      <h4>Responsive Portfolio</h4>
+      <a href="http://carlosdvp.github.io/Responsive-Portfolio/" data-toggle="tooltip" title="Responsive Portfolio Project" target="_blank">
+        <img class="gallery-image img-thumbnail" src="../assets/Responsive-Portfolio.jpg" alt="Responsive Portfolio Project">
+      </a>
+      <div class="card-body">
+        <h5 class="">Responsive Portfolio Project</h5>
+        <p class="card-text">Santa Cruz de la Sierra, Bolivia</p>
+      </div>
+      <div class="footer">
+        <a href="http://carlosdvp.github.io/Responsive-Portfolio/" title="Responsive Portfolio Project" target="_blank">Visit the Project</a>
+        
+      </div>
+    </div>
+
+    <div>
+      <h4>Website Project 3</h4>
+      <a href="https://carlosdvp.github.io/Website-project-3/" data-toggle="tooltip" title="Portfolio Project Number 3" target="_blank">
+        <img class="gallery-image img-thumbnail" src="../assets/Website-project-3.jpg" alt="Portfolio Poject 3">
+      </a>
+      <div>
+        <h5>Portfolio Website Number 3</h5>
+        <p>Falls Church, VA</p>
+      </div>
+      <div>
+        <a href="https://carlosdvp.github.io/Website-project-3/" title="Portfolio Project Number 3" target="_blank">Visit the Project</a>
+      </div>
+    </div>
+
+    <div>
+      <h4>JS Basic Chatbot Project</h4>
+      <a href="https://carlosdvp.github.io/First-bot-Chatbot/" data-toggle="tooltip" title="Basic JS chatbot" target="_blank">
+        <img class="gallery-image img-thumbnail" src="../assets/basic-chatbot.png" alt="Js chatbot Project">
+      </a>
+      <div>
+        <h5>JS Project</h5>
+        <p>Basic Chatbot App written in JavaScript</p>
+      </div>
+      <div>
+        <a href="http://carlosdvp.github.io/Udacity-Resume-Project/" title="Udacity Js Project" target="_blank">Visit the Project</a>
       </div>
     </div>
 
@@ -116,29 +108,33 @@ export default {
   
 .main-gallery {
   margin: 0 auto;
+  padding: 6rem 0;
+  background:  linear-gradient(
+    rgba(50, 100, 100, .7),
+    rgba(50, 100, 100, .7)
+  ),
+  url('../assets/splatter.jpg');
+  background-repeat: round;
+  background-size: contain;
+  color: ghostwhite;
 }
 
 .gallery-image {
   background: rgba(12, 77, 105, .2);
-}
-
-#gallery-image-1 {
-  width: 100%;
+  width: 80%;
   height: 50vh;
   object-fit: cover;
 }
 
-.card .btn {
-  width: 100%;
-}
-
-.card-deck {
+div {
   margin-bottom: 2em;
   margin-top: 2em;
 }
  
 .img-thumbnail {
   border: 2px solid ghostwhite;
+  border-radius: 15px;
+  padding: 1em;
 }
 
 </style>
